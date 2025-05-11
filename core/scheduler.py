afd365bc403ec9de5e69d26ceb8bfb692fb16758
Cioban George-Adrian <cgeorgeadrian99@gmail.com>
2025-05-11 09:49:26 +0200

    small fix

--- core/scheduler.py
+++ core/scheduler.py
@@ -9,11 +9,10 @@ from sapphirepy import sapphire
 import json
 from pathlib import Path
 from typing import Union
-from MainContract import ComputeTask
 from src.SubContract import Crumb, CrumbStatus, update_crumb_to_closed
 from core.transformer_task import TransformerTask
 from src.SubContract import get_crumbs_by_status, get_crumbs_by_requester
-from src.MainContract import get_in_progress_queue
+from src.MainContract import get_in_progress_queue, ComputeTask
 
 
 class Orchestrator:
@@ -35,7 +34,6 @@ class Orchestrator:
         private_key_value: str = private_key_file.read()
 
         os.environ.setdefault("PRIVATE_KEY", private_key_value)
-        print("KEY: " + str(os.environ.get("PRIVATE_KEY")))
         account: LocalAccount = Account.from_key(private_key_value)
 
         w3 = AsyncWeb3(
@@ -79,14 +77,24 @@ class Orchestrator:
 
     async def fetch_job(self):
         # Get crumbs that have been selected for work
-        MAIN_CONTRACT_ADDR = "0x123f578600F8B64B235ba9D627F121c619731275"
+        MAIN_CONTRACT_ADDR = "0x885cA90bD752A682dD1883614edA0C0557c973a6"
         all_subcontracts: list[ComputeTask] = await get_in_progress_queue(MAIN_CONTRACT_ADDR)
         for contract in all_subcontracts:
             available_crumbs: list[Crumb] = await get_crumbs_by_requester(contract.subContractAddress)
             if len(available_crumbs) > 0:
-                self.current_job = available_crumbs[0]
-                self.selected_contract = contract.subContractAddress
-
+                for crumb in available_crumbs:
+                    if crumb.status.value == CrumbStatus.QUEUED.value:
+                        self.current_job = crumb
+                        self.selected_contract = contract.subContractAddress
+                        break
+                if self.current_job is None:
+                    continue
+                # Shameful hack to get the setup_task
+                self.current_job.setup_task = json.loads(
+                    self.current_job.setup_task)
+                self.current_job.setup_validation = json.loads(
+                    self.current_job.setup_validation)
+                break
         return self.current_job is not None
 
     async def publish_job_results(self, result: str) -> bool:
@@ -108,10 +116,11 @@ async def start_orchestrator(
     orchestrator = Orchestrator(network, contract,  pkey)
 
     print("Starting recurring task for fetching jobs...")
-    while await orchestrator.fetch_job():
+    while True:
+        await orchestrator.fetch_job()
         if orchestrator.current_job is None:
-            raise Exception(
-                "A problem occured and currently selected job is None")
+            await sleep(30)
+            continue
         task = TransformerTask()
         task.set_params(
             orchestrator.current_job.setup_task)
@@ -120,4 +129,4 @@ async def start_orchestrator(
         result_str = str(task.get_results())
         await orchestrator.publish_job_results(result_str)
 
-        await sleep(10)
+        await sleep(30)
